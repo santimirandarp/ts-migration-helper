f4f007ebb84fc5c7709470c39b298e0dc3d1adc6
Santi Miranda <santimir@protonmail.com>
2023-08-04 22:02:57 +0100

    fix: remove unused file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
 #!/usr/bin/env node
 import chalk from 'chalk';
 
-import { run } from './run.js';
+import { existsSync } from 'node:fs';
 
-run()
+import {
+  installSoftware,
+  configureSoftware,
+  updatePackageJson,
+  updateGitignore,
+  removeOld,
+  replaceWorkflow,
+  npmCheckUpdates,
+} from './tasks/index.js';
+/**
+ * Please update if the guide changes.
+ * https://github.com/cheminfo/generator-cheminfo/blob/main/ts-migration.md
+ */
+(async function run() {
+  if (!existsSync('package.json')) {
+    console.error(
+      '\npackage.json not found. Run this program in the project root.',
+    );
+    process.exit(1);
+  }
+  await npmCheckUpdates();
+  await removeOld();
+  await installSoftware();
+  await configureSoftware();
+  await updateGitignore();
+  await updatePackageJson();
+  await replaceWorkflow();
+})()
   .then(() => {
     console.log(
       chalk.green('Done!'),
